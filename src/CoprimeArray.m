2fbfbb5b8b6cb5c0a435018506ac3afa83e1e09c
Pablo Johnson <paulj.96@gmail.com>
2019-01-28 20:34:29 -0600

    Followed up with changes to make it dependent on max sensor rather than # of periods

--- src/CoprimeArray.m
+++ src/CoprimeArray.m
@@ -3,9 +3,9 @@ function Subarray = CoprimeArray(N,M,max_sensor)
 % N, and the number of periods. Also returns the subarrays and the
 % available lags and number of each lag. 
 %%
-% M = 3;
-% N = 4;
-% periods = 2;
+M = 2;
+N = 3;
+max_sensor = 64;
 
 
 
@@ -13,11 +13,11 @@ Subarray.max_sensor = max_sensor;
 
 % Generate 0 1 representation of subarray 1 and 2
 Subarray1 = zeros(1,max_sensor);
-Subarray1(M:M:max_sensor) = 1;
-Subarray1 = [1,Subarray1];
+Subarray1((0:N:(max_sensor-1))+1) = 1;
+% Subarray1 = [1,Subarray1];
 Subarray2 = zeros(1,max_sensor);
-Subarray2(N:N:max_sensor) = 1;
-Subarray2 = [1,Subarray2];
+Subarray2((0:M:(max_sensor-1))+1) = 1;
+% Subarray2 = [1,Subarray2];
 
 
 Sensor_placement = max(vertcat(Subarray1, Subarray2)); % Combine two subarrays
@@ -29,7 +29,7 @@ Subarray.sub1 = Subarray1;
 Subarray.sub2 = Subarray2;
 
 % Find the available lags given our coprime array
-lags = zeros(1,max_sensor+1); 
+lags = zeros(1,max_sensor); 
 for i = 1:length(Sensor_placement) 
     if Sensor_placement(i) == 1 
         for k = i:length(Sensor_placement) 
@@ -39,7 +39,7 @@ for i = 1:length(Sensor_placement)
         end
     end
 end
-lags = [0:max_sensor; lags];
+lags = [0:max_sensor-1; lags];
 Subarray.lags = lags;
 
 end
