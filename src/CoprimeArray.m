ee5ef3ebb1efe469c81105b182992a93cd9ca0eb
Pablo Johnson <paulj.96@gmail.com>
2019-01-28 20:13:55 -0600

    Changed CoprimeArray to use max sensor vs # of periods

--- src/CoprimeArray.m
+++ src/CoprimeArray.m
@@ -1,4 +1,4 @@
-function Subarray = CoprimeArray(M,N,periods)
+function Subarray = CoprimeArray(M,N,max_sensor)
 % Function to generate vector representation of the coprime array given M,
 % N, and the number of periods. Also returns the subarrays and the
 % available lags and number of each lag. 
@@ -8,17 +8,17 @@ function Subarray = CoprimeArray(M,N,periods)
 % periods = 2;
 
 
-M_N_max = [(periods*N-1)*M,(periods*M-1)*N]; % Calculate the max of each subarray
-Subarray.M_N_max = M_N_max;
-max_sensor = max(M_N_max); % Highest sensor number
+
+Subarray.max_sensor = max_sensor;
 
 % Generate 0 1 representation of subarray 1 and 2
 Subarray1 = zeros(1,max_sensor);
-Subarray1(M:M:M_N_max(1)) = 1;
+Subarray1(M:M:max_sensor) = 1;
 Subarray1 = [1,Subarray1];
 Subarray2 = zeros(1,max_sensor);
-Subarray2(N:N:M_N_max(2)) = 1;
+Subarray2(N:N:max_sensor) = 1;
 Subarray2 = [1,Subarray2];
+
 
 Sensor_placement = max(vertcat(Subarray1, Subarray2)); % Combine two subarrays
 
