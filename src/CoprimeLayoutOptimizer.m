197f402c0701248b5ba713ca538bf87ea33c90b7
JohnHamiltonWillis <33499342+JohnHamiltonWillis@users.noreply.github.com>
2019-01-29 17:08:34 -0600

    Update CoprimeLayoutOptimizer.m
    Included functionality for producing all possible coprime arrays for a sensor array.

--- src/CoprimeLayoutOptimizer.m
+++ src/CoprimeLayoutOptimizer.m
@@ -3,6 +3,14 @@ function [coprime_array] = CoprimeLayoutOptimizer(sensor_layout)
 %given a sparse array layout indicating which sensors have available data
 %with a 1 or 0 for none. Current function is supported with data for up to
 %64 sensors
+if length(sensor_layout) >= 64
+    error('sensor_layout too long. Use 64 or less sensors.');
+end
+cpairs = [];
 for spacing = 1:63
-    cpairs(spacing) = GenerateCoprimePairs(0,64, spacing)
+    cpairs = GenerateCoprimePairs(2,64, spacing);
+    for pair = 1:length(cpairs)
+        CoprimeArray(cpairs{pair}(1),cpairs{pair}(2),64)
+        %then do sensor_layout + CoprimeArray' RESUME PROGRESS HERE
+    end
 end
