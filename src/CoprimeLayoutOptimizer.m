23de9429ccb9835a008824f16d4cee8779921db9
JohnHamiltonWillis <33499342+JohnHamiltonWillis@users.noreply.github.com>
2019-02-05 16:53:10 -0600

    Update CoprimeLayoutOptimizer.m
    Completed functionality, but pulling data function currently not included. This function currently doesn't support every coprime array configuration.

--- src/CoprimeLayoutOptimizer.m
+++ src/CoprimeLayoutOptimizer.m
@@ -1,16 +1,29 @@
-function [coprime_array] = CoprimeLayoutOptimizer(sensor_layout)
-%Function to use the best possible coprime pairs and number of periods 
-%given a sparse array layout indicating which sensors have available data
-%with a 1 or 0 for none. Current function is supported with data for up to
-%64 sensors
-if length(sensor_layout) >= 64
-    error('sensor_layout too long. Use 64 or less sensors.');
-end
+function [coprime_array] = CoprimeLayoutOptimizer(sensor_layout,sensors)
+% Function to use the best possible coprime pairs to reduce PSL height. 
+% This function currently only generates coprime arrays starting at the
+% first sensor in the array and only generates subarrays that have sensors
+% available for full extension to the last sensor
 cpairs = [];
+coprime_spec = [];
 for spacing = 1:63
+    % Iterate through all the coprime pair spacings and generate pairs
     cpairs = GenerateCoprimePairs(2,64, spacing);
     for pair = 1:length(cpairs)
-        CoprimeArray(cpairs{pair}(1),cpairs{pair}(2),64)
-        %then do sensor_layout + CoprimeArray' RESUME PROGRESS HERE
+        for max_sensor = 2:64
+            % Create the layout necessary given a comprime pair and number
+            % of sensors
+            subarray = CoprimeArray(cpairs{pair}(1),cpairs{pair}(2),max_sensor)
+            % See if subarray for a coprime pair fits in available sensors
+            if (sensor_layout & ~subarray)
+                %pull PSL data
+                temp = [2 3 13.25]; %temp = (pull data from table)
+                % Store the highest PSL difference
+                if temp(3) > coprime_spec(3)
+                    coprime_spec = temp;
+                end
+            end
+        end
     end
+end
+coprime_array = CoprimeArray(coprime_spec(1),coprime_spec(2),sensors);
 end
