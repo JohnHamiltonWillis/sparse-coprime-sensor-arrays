be30a4ff9b1bbcc4e87c96138c18ade52ef808b8
JohnHamiltonWillis <33499342+JohnHamiltonWillis@users.noreply.github.com>
2019-02-24 20:00:09 -0600

    Added functionality for checking shifted coprime layouts

--- src/CoprimeLayoutOptimizer.m
+++ src/CoprimeLayoutOptimizer.m
@@ -1,29 +1,23 @@
-function [coprime_array] = CoprimeLayoutOptimizer(sensor_layout,sensors)
+function [opt_coprime_array] = CoprimeLayoutOptimizer(sensor_layout)
 % Function to use the best possible coprime pairs to reduce PSL height. 
 % This function currently only generates coprime arrays starting at the
 % first sensor in the array and only generates subarrays that have sensors
 % available for full extension to the last sensor
-cpairs = [];
-coprime_spec = [];
+coprime_spec = [0 0 0];
 for spacing = 1:63
     % Iterate through all the coprime pair spacings and generate pairs
     cpairs = GenerateCoprimePairs(2,64, spacing);
     for pair = 1:length(cpairs)
-        for max_sensor = 2:64
+        for max_sensor = 3:64
             % Create the layout necessary given a comprime pair and number
             % of sensors
-            subarray = CoprimeArray(cpairs{pair}(1),cpairs{pair}(2),max_sensor)
-            % See if subarray for a coprime pair fits in available sensors
-            if (sensor_layout & ~subarray)
-                %pull PSL data
-                temp = [2 3 13.25]; %temp = (pull data from table)
-                % Store the highest PSL difference
-                if temp(3) > coprime_spec(3)
-                    coprime_spec = temp;
-                end
+            coprime_array = CoprimeArray(cpairs{pair}(1),cpairs{pair}(2),max_sensor);
+            coprime_array = coprime_array.array;
+            % Shift the array across the full length of the array
+            for shift = 0:(63-length(coprime_array))
+                coprime_array = [0 coprime_array(1:(length(coprime_array)-1))];
             end
         end
     end
 end
-coprime_array = CoprimeArray(coprime_spec(1),coprime_spec(2),sensors);
-end
+opt_coprime_array = CoprimeArray(coprime_spec(1),coprime_spec(2),64);
