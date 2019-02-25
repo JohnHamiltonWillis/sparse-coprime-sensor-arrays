dd0b40559a2a5ace4b8f0a3c906dcee6be8c456a
JohnHamiltonWillis <33499342+JohnHamiltonWillis@users.noreply.github.com>
2019-02-24 22:55:30 -0600

    Readied functionality for loading comparison data

--- src/CoprimeLayoutOptimizer.m
+++ src/CoprimeLayoutOptimizer.m
@@ -1,21 +1,25 @@
 function [opt_coprime_array] = CoprimeLayoutOptimizer(sensor_layout)
 % Function to use to determine the best possible coprime pairs to reduce
 % PSL height. This function currently isn't hooked up to the data required 
-% for comparisons. Once data is in a more readable format, this algorithm
-% will be updated.
-coprime_spec = [0 0 0];
+% for comparisons. Once data is finished generating, this function will be
+% updated.
+opt_coprime_spec = [0 0 0];
 for spacing = 1:63
     % Iterate through all the coprime pair spacings and generate pairs
     cpairs = GenerateCoprimePairs(2,64, spacing);
     for pair = 1:length(cpairs)
-        for max_sensor = 3:64
-            % Create the layout necessary given a comprime pair and number
-            % of sensors
+        % Iterate through all the coprime pairs
+        for period = 1:floor(64/cpairs{pair}(2))
+            % Iterate through all the period extensions that fit in the 64
+            % sensor array
+            max_sensor = period*cpairs{pair}(2);
+            % Create the coprime layout necessary for a given coprime pair
             coprime_array = CoprimeArray(cpairs{pair}(1),cpairs{pair}(2),max_sensor);
             coprime_array = coprime_array.array;
-            % Shift the array across the full length of the array
-            for shift = 0:(63-length(coprime_array))
-                coprime_array = [0 coprime_array(1:(length(coprime_array)-1))];
+            for shift = 1:(64-length(coprime_array))
+                % Shift coprime_array from the beginning to the end of the
+                % 64 sensor array
+                coprime_array = [0 coprime_array];
                 % Check if the proper sensors are available for coprime
                 % array
                 eligible = true;
@@ -25,17 +29,18 @@ for spacing = 1:63
                         break
                     end
                 end
-                % Store best coprime layout
-                if eligible == true
-                    %pull PSL data
-                    temp = [2 3 13.25]; %temp = (pull data from table)
-                    % Store the highest PSL difference
-                    if temp(3) > coprime_spec(3)
-                        coprime_spec = temp;
-                    end
+            end
+            % Store best coprime layout
+            if eligible == true
+                % Pull PSL data
+                load(['C:\Users\Work\Downloads\2_100_' num2str(spacing) '.mat'], 'Z')
+                coprime_spec = Z(pair, period);
+                % Store the highest PSL difference
+                if coprime_spec(3) > opt_coprime_spec(3)
+                    opt_coprime_spec = coprime_spec;
                 end
             end
         end
     end
 end
-opt_coprime_array = CoprimeArray(coprime_spec(1),coprime_spec(2),64);
+opt_coprime_array = CoprimeArray(opt_coprime_spec(1),opt_coprime_spec(2),64);
