9b8cb6a31a482ac26d370703e8792934044e2513
Pablo Johnson <paulj.96@gmail.com>
2019-03-27 10:26:42 -0500

    started work

---
+++ src/all_csa_arrangements.m
@@ -0,0 +1,8 @@
+for spacing = 1:64
+    coprime_pairs = GenerateCoprimePairs(2,64,spacing);
+    for pair = 1:length(coprime_pairs)
+        U1 = coprime_pairs{pair}(1);
+        U2 = coprime_pairs{pair}(2);
+        
+    end
+end
