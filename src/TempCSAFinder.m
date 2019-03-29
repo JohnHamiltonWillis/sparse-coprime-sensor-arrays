35751d68b93bdae080ad771999f1a1f5139be390
JohnHamiltonWillis <33499342+JohnHamiltonWillis@users.noreply.github.com>
2019-03-29 12:15:52 -0500

    Created TempCSAFinder
    This function currently finds all the possible coprime sensor layouts with equal extensions of subarrays.

---
+++ src/TempCSAFinder.m
@@ -0,0 +1,41 @@
+function [coprime_arrays] = TempCSAFinder(sensor_layout,array_length)
+% sensor_layout is a vector of 1's and 0's where a 1 indicates that a 
+% sensor is available at that position. array_length is the total number of
+% sensor positions in the array. This function finds all the possible 
+% coprime sensor array layouts assuming equal extension of subarrays that 
+% will fit in any given sparse linear array. This function will likely be 
+% repurposed later for higher functionality.
+coprime_arrays = [];
+for spacing = 1:(array_length-1)
+    % Iterate through all the coprime pair spacings and generate pairs
+    cpairs = GenerateCoprimePairs(2,array_length, spacing);
+    for pair = 1:length(cpairs)
+        % Iterate through all the coprime pairs
+        for period = 1:floor(array_length/cpairs{pair}(2))
+            % Iterate through all the period extensions that fit in the
+            % full sensor array
+            max_sensor = period*cpairs{pair}(2);
+            % Create the coprime layout necessary for a given coprime pair
+            coprime_array = CoprimeArray(cpairs{pair}(1),cpairs{pair}(2),max_sensor);
+            coprime_array = coprime_array.array;
+            for shift = 1:(array_length-length(coprime_array))
+                % Shift coprime_array from the beginning to the end of the
+                % full sensor array
+                coprime_array = [0 coprime_array];
+                % Check if the proper sensors are available for coprime
+                % array
+                eligible = true;
+                for sensor = 1:length(coprime_array)             
+                    if ((coprime_array(sensor)==1) && (sensor_layout(sensor)==0))
+                        eligible = false;
+                        break
+                    end
+                end
+            end
+            % Store eligible coprime layout
+            if eligible == true
+                coprime_arrays = [coprime_arrays; cpairs{pair}(1) cpairs{pair}(2) max_sensor];
+            end
+        end
+    end
+end
