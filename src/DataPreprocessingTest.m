860363717be4e4972224288754922b2bbb19a580
JohnHamiltonWillis <33499342+JohnHamiltonWillis@users.noreply.github.com>
2019-04-09 18:03:32 -0500

    Updated DataPreProcessingTest
    This function will eliminate the extra exponential produced from a sinusoid source for our 64 sensor array

--- src/DataPreprocessingTest.m
+++ src/DataPreprocessingTest.m
@@ -1,28 +1,6 @@
-clear
-SNRdB = -10; SampleSize = 1e3;
-us = cosd(randi(181,[1 2])-1);%%%Directions are uniformly distributed from 0 to 180 degrees
-numSources = length(us);
-lambda = 50;    d = lambda/2;    kx = 2*pi/lambda * us;
-%%%calculate noise variance for signal power 1
-vars = ones(1,numSources);
-varn = vars(1)*10^(-SNRdB/10);
-s = zeros(numSources,SampleSize);
-%%input signals: proper GaussianK0478768904
-for idx = 1:numSources
-    s(idx,:) = (sqrt(vars(idx)/2)*randn(1,SampleSize) + 1i*sqrt(vars(idx)/2)*randn(1,SampleSize));
-end
+function DataPreprocessingTest(x)
+SampleSize = length(x(1,:));
 ApertureEnd = 63;%%%%The array starts at 0 and ends at 63
-
-%%steering vector
-v = zeros(ApertureEnd+1,numSources);
-%%%%%The vector will have the data for all sensors
-x = zeros(ApertureEnd+1,SampleSize);
-for idx = 1:numSources
-    v(:,idx) = exp(1i*kx(idx)*(0:ApertureEnd)*d).';
-    x = x + v(:,idx)*s(idx,:);
-end
-%%%Add proper Gaussian noise samples
-x = x + sqrt(varn/2)*randn(ApertureEnd+1,SampleSize) + 1i*sqrt(varn/2)*randn(ApertureEnd+1,SampleSize);
 
 %Fourier transform trimming
 F = zeros(ApertureEnd+1,SampleSize);
@@ -30,9 +8,29 @@ S = zeros(ApertureEnd+1,SampleSize);
 Strim = zeros(ApertureEnd+1,SampleSize);
 xtrim = zeros(ApertureEnd+1,SampleSize);
 for idx = 1:ApertureEnd+1
-    F(idx,:) = fft(x(idx,:));
+    F(idx,:) = fft(x(idx,:),SampleSize);
     S(idx,:) = fftshift(F(idx,:));
     Strim(idx,:) = [zeros(1,SampleSize/2) S(idx,SampleSize/2+1:SampleSize)];
     xtrim(idx,:) = ifft(Strim(idx,:));
+end
+w = linspace(-1, 1, length(S));
+A = fftshift(fft(x(1,:)));
+A = A/max(abs(A));
+subplot(2,1,1)
+plot(w,20*log10(abs(A)))
+xlim([-0.25, 0.25]);
+ylim([-30, 0]);
+xlabel('x\pi, frequency (rad/s)')
+ylabel('S_x(exp(j\omega)) (dB)')
+grid on
+B = fft(xtrim(1,:));
+B = B/max(abs(B));
+subplot(2,1,2)
+plot(w,20*log10(abs(B)))
+xlim([-0.25, 0.25]);
+ylim([-30, 0]);
+xlabel('x\pi, frequency (rad/s)')
+ylabel('S_x(exp(j\omega)) (dB)')
+grid on
 end
 
